323e6ad0cb5b71133e2e68ac4c6bb4a57d30374b
Vanessa Targino <nessaoli1996@gmail.com>
2025-07-16 12:46:46 -0300

    Remove submódulo farmacia e adiciona como pasta normal

---
+++ farmacia/src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'Pdg@@2022',
+      database: 'db_crudfarmacia',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
